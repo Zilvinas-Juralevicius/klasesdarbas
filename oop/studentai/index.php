87309299c6815566efbb1584cfa994a12316f54a
Zilvinas Juralevicius <zilvinas.juralevicius@gmail.com>
2020-10-27 20:38:51 +0200

    5 savaites pradzia

--- oop/studentai/index.php
+++ oop/studentai/index.php
@@ -1,16 +1,25 @@
 <?php
 
-require_once 'Studentas.php';
-require_once 'Spausdinimas.php';
-require_once 'Grupe.php';
+use KCS\Grupe;
+use KCS\Helper;
+use KCS\Spausdinimas;
+use KCS\Studentas;
 
-$studentai = [];
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+$log = new Monolog\Logger('Studentas');
+$log->pushHandler(new Monolog\Handler\StreamHandler('../../logs/studentasapp.log', Monolog\Logger::INFO));
+$log->info('Aplikacija pradejo darba');
+
+
+$studentaiM = [];
+$studentaiV = [];
 $studentuDuomenys = [
-    ['name' => 'Jonas', 'surename' => 'Jonaitis', 'groupId' => 777, 'code' => 38765432101],
-    ['name' => 'Petras', 'surename' => 'petraitis', 'groupId' => 777, 'code' => 38945612304],
-    ['name' => 'Ona', 'surename' => 'Oniene', 'groupId' => 777, 'code' => 45432198710],
-    ['name' => 'Zita', 'surename' => 'Zitaite', 'groupId' => 777, 'code' => 64185296325],
-    ['name' => 'Kestas', 'surename' => 'Kestaitis', 'groupId' => 777, 'code' => 55768921402],
+    ['name' => 'Jonas', 'surename' => 'Jonaitis', 'groupId' => 777, 'code' => 39012120001],
+    ['name' => 'Petras', 'surename' => 'petraitis', 'groupId' => 777, 'code' => 52010120001],
+    ['name' => 'Ona', 'surename' => 'Oniene', 'groupId' => 777, 'code' => 49112120001],
+    ['name' => 'Zita', 'surename' => 'Zitaite', 'groupId' => 777, 'code' => 61512120001],
+    ['name' => 'Kestas', 'surename' => 'Kestaitis', 'groupId' => 777, 'code' => 35712121402],
 ];
 
 foreach ($studentuDuomenys as $studentoDuomenys) {
@@ -22,7 +31,16 @@ foreach ($studentuDuomenys as $studentoDuomenys) {
     $studentas->setGroup($grupe);
     $studentas->setCode($studentoDuomenys['code']);
 
-    $studentai[] = $studentas;
+    if (Helper::arVyras($studentas)) {
+        $studentaiV[] = $studentas;
+    } else {
+        $studentaiM[] = $studentas;
+    }
 }
+echo "Vyrai:";
+Spausdinimas::spausdintiHtml($studentaiV);
+echo "Moterys:";
+Spausdinimas::spausdintiHtml($studentaiM);
 
-Spausdinimas::spausdintiHtml($studentai);
+var_dump(Helper::rastiJauniausiaStudenta($studentaiV));
+var_dump(Helper::rastiVyriausiaStudenta($studentaiV));
